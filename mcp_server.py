d7bbf9ae028e095383b0453e515b5d10c1dd652b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2026-05-25 20:03:23 -0300

    feat: defining resources

--- mcp_server.py
+++ mcp_server.py
@@ -16,7 +16,7 @@ docs = {
 
 
 
-# TODO: Write a resource to return the contents of a particular doc
+
 # TODO: Write a prompt to rewrite a doc in markdown format
 # TODO: Write a prompt to summarize a doc
 
@@ -53,12 +53,12 @@ def edit_document(
 
     docs[doc_id] = docs[doc_id].replace(old_str, new_str)
 
-# TODO: Write a resource to return all doc id's
+#  Write a resource to return all doc id's
 @mcp.resource("docs://documents", mime_type="application/json")
 def list_docs() -> list[str]:
     return list(docs.keys())
 
-
+#  Write a resource to return the contents of a particular doc
 @mcp.resource("docs://documents/{doc_id}", mime_type="text/plain")
 def fetch_doc(doc_id: str) -> str:
     if doc_id not in docs:
